1fc3f195a264f03257a032aae1dab36fe68ef2f2
Webizito <31759403+Webizito@users.noreply.github.com>
2026-05-31 00:05:39 +0530

    Wordpress latest Version
     Wordpress latest Version supported. Upgrade immediately.

--- tags/3.1/online-active-users.php
+++ tags/3.1/online-active-users.php
@@ -4,8 +4,8 @@
  * Plugin Title: Online Active Users Plugin
  * Plugin URI: https://wordpress.org/plugins/online-active-users/
  * Description: Monitor and display real-time online users and last seen status on your WordPress site with WP Online Active Users plugin.
- * Tags: online users, active users, online active users, real-time users, user activity
- * Version: 3.3
+ * Tags: wp-online-active-users, online-user, active-users, woocommerce-users, user-status
+ * Version: 3.1
  * Author: Webizito
  * Author URI: http://webizito.com/
  * Contributors: valani9099
